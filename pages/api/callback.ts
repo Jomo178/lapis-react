67b7120b043b18af3960afcad6040bd798003643
Jomo178 <shahbazi8@outlook.de>
2023-04-02 18:09:19 +0200

    update

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Base64, DiscordFetch } from "lib/functions";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 const Verify = async (req: NextApiRequest, res: NextApiResponse) => {
   const { code, state } = req.query as Record<string, string>;
@@ -22,13 +23,23 @@ const Verify = async (req: NextApiRequest, res: NextApiResponse) => {
       .status(400)
       .send({ status: 400, message: "something went wrong" });
 
-  let token = "";
+  const response = NextResponse.next();
 
-  const userID = Base64.encode(fetchUsersData.id);
-  const time = new Date().getTime() - 1_293_840_000;
-  const date = Base64.encode(time.toString());
+  const nowTime = new Date();
+  const randCode = Math.random().toString(36).substring(2);
+  const timeCookie = nowTime.getTime() - 1_293_840_000;
 
-  res.send({ userID, date, fetchUsersData });
+  let cookieToken = `${Base64.encode(fetchUsersData.id)}.${Base64.encode(
+    timeCookie.toString()
+  )}.${Base64.encode(randCode)}`;
+
+  response.cookies.set({
+    name: "token",
+    value: cookieToken,
+    maxAge: 60000 * 60 * 24 * 7,
+  });
+
+  res.send({ fetchUsersData });
 };
 
 export default Verify;
